99289492846a655b0a9146e65701d30327104c25
taramozart <taramozart666@gmail.com>
2022-12-12 21:09:02 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+function intToRoman($num) {
+    $arrA =  
+    ['M' => 1000, 
+    'CM' => 900, 
+    'D' => 500, 
+    'CD' => 400, 
+    'C' => 100, 
+    'XC' => 90, 
+    'L' => 50, 
+    'XL' => 40, 
+    'X' => 10, 
+    'IX' => 9, 
+    'V' => 5, 
+    'IV' => 4, 
+    'I' => 1];
+
+    $result = '';
+
+    while ($num > 0) {
+        foreach ($arrA as $romawi => $int) {
+            if($num >= $int) {
+                $num -= $int;
+                $result .= $romawi;
+                break;
+            }
+        }
+    }
+    echo $result;
+}
+echo intToRoman(1994);
+?>
